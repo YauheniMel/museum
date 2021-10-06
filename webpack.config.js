57b4a908303cc8d5b27521eb1df504937549a7f5
YauheniMel <207melnik@gmail.com>
2021-10-06 19:52:28 +0300

    init: configuration project

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new HTMLWebpackPlugin({
+      template: './src/tour7.html',
+      filename: './tour7.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.ttf$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext]',
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[hash][ext]',
+        },
+      },
+    ],
+  },
+};
